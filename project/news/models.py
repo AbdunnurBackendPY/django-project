afab273fe882f7979ae64ef14112ea9b00cf12d2
AbdunnurBackendPY <146436086+AbdunnurBackendPY@users.noreply.github.com>
2025-01-27 22:33:15 +0600

    все заебись

---
+++ project/news/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+class Articles(models.Model):
+    user = models.ForeignKey(User,
+        verbose_name='Author',
+        on_delete=models.CASCADE
+    )
+    title = models.CharField('Name', max_length=50)
+    anons = models.CharField('Anons', max_length=250)
+    full_text = models.TextField('Article')
+    date = models.DateTimeField('Publication date')
+
+    def __str__(self):
+        return self.title
