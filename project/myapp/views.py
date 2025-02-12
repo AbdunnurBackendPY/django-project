c4cb054c22fef9e5b1e3f5312db86b3bd5e75e46
AbdunnurBackendPY <146436086+AbdunnurBackendPY@users.noreply.github.com>
2025-02-12 22:52:29 +0600

    bootstrap

--- project/myapp/views.py
+++ project/myapp/views.py
@@ -3,11 +3,3 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'myapp/home.html')
-
-
-def about(request):
-    return render(request, 'myapp/about.html')
-
-
-def contacts(request):
-    return render(request, 'myapp/contacts.html')
